ecc686fed86d1556deef0a49c62f8b55e41c9689
ANGGALMUIS <anggalmuis@gmail.com>
2026-06-06 15:12:42 +0700

    test minimal app

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -1,49 +1,12 @@
-import dotenv from "dotenv";
-dotenv.config();
-
 import express from "express";
-import cors from "cors";
-import authRoutes from "./routes/auth.routes.js";
-import testRoutes from "./routes/test.routes.js";
-import adminRoutes from "./routes/admin.routes.js";
-import canteenRoutes from "./routes/canteen.routes.js";
-import menuRoutes from "./routes/menu.routes.js";
-import orderRoutes from "./routes/order.routes.js";
-import { errorHandler } from "./middleware/error.middleware.js";
-import helmet from "helmet";
-import { apiLimiter } from "./middleware/rateLimit.middleware.js";
-import swaggerUi from "swagger-ui-express";
-import swaggerSpec from "./config/swagger.js";
-import uploadRoutes from "./routes/upload.routes.js";
-import categoryRoutes from "./routes/category.routes.js";
-import canteenRequestRoutes from "./routes/canteenRequest.routes.js";
 
 const app = express();
-app.use((req, res, next) => {
-  console.log(`${req.method} ${req.url}`);
-  next();
-});
 
-app.use(cors());
-app.use(express.json());
-app.use("/api/auth/login", apiLimiter);
-app.use("/api/auth/register", apiLimiter);
-app.use("/api/admin", adminRoutes);
-app.use("/api/auth", authRoutes);
-app.use("/api/test", testRoutes);
-app.use("/api/canteens", canteenRoutes);
-app.use("/api/categories", categoryRoutes);
-app.use("/api/canteen-requests", canteenRequestRoutes);
-app.use("/api/menus", menuRoutes);
-app.use("/api/orders", orderRoutes);
-app.use(helmet());
-app.use("/api/upload", uploadRoutes);
-app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 app.get("/", (req, res) => {
   res.json({
-    message: "Kantin Polines API Running",
+    success: true,
+    message: "Railway Test Success",
   });
 });
-app.use(errorHandler);
 
 export default app;
