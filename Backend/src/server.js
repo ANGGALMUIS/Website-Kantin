d19d08817ec94d0cdbdb32e69969b429f49c09cb
ANGGALMUIS <anggalmuis@gmail.com>
2026-06-06 15:28:34 +0700

    railway test

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -1,46 +1,20 @@
-import dotenv from "dotenv";
-dotenv.config();
-
 import http from "http";
-import { Server } from "socket.io";
 
-import app from "./app.js";
+const PORT = process.env.PORT || 8080;
 
-const PORT = process.env.PORT || 3000;
-
-const server = http.createServer(app);
-
-export const io = new Server(server, {
-  cors: {
-    origin: "*",
-    methods: ["GET", "POST", "PATCH", "DELETE"],
-  },
-});
-
-io.on("connection", (socket) => {
-  console.log(`User Connected: ${socket.id}`);
-
-  // Room untuk kantin
-  socket.on("join-canteen", (canteenId) => {
-    console.log(`JOIN CANTEEN ROOM => canteen_${canteenId}`);
-    socket.join(`canteen_${canteenId}`);
-
-    console.log(`Socket ${socket.id} joined canteen_${canteenId}`);
+const server = http.createServer((req, res) => {
+  res.writeHead(200, {
+    "Content-Type": "application/json",
   });
 
-  // Room untuk buyer
-  socket.on("join-buyer", (buyerId) => {
-    console.log(`JOIN BUYER ROOM => buyer_${buyerId}`);
-    socket.join(`buyer_${buyerId}`);
-
-    console.log(`Socket ${socket.id} joined buyer_${buyerId}`);
-  });
-
-  socket.on("disconnect", () => {
-    console.log(`User Disconnected: ${socket.id}`);
-  });
+  res.end(
+    JSON.stringify({
+      success: true,
+      message: "Railway Alive",
+    }),
+  );
 });
 
 server.listen(PORT, "0.0.0.0", () => {
-  console.log(`Server running on port ${PORT}`);
+  console.log(`Listening on ${PORT}`);
 });
